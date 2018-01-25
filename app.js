a9c7fb7767401e7d570421c432fa85d7f2ab3809
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-01-25 18:33:05 +0530

    separate modules for UI, data and controller

---
+++ app.js
@@ -0,0 +1,34 @@
+// Data Module ->
+
+var budgetController = (function() {
+    var x = 23;
+    var add = function(a) {
+        return x + a;
+    }
+    
+    return {
+        publicTest: function(b) {
+            return add(b);
+        }
+    }
+})();
+
+// User Interface Module ->
+
+var UIController = (function() {
+    
+})
+
+// Controller Module ->
+
+var controller = (function(budgetCtrl, UICtrl){ 
+    
+    var z = budgetCtrl.publicTest(5);
+    
+    return {
+        anotherPublic: function() {
+            console.log(z);
+        }
+    }
+    
+})(budgetController, UIController);
